e77f4709a8e99fcaf52503b6065dafdbf63ed019
elbekdamirov <elbekmain@gmail.com>
2025-03-24 21:06:52 +0500

    4 va 5 qoldi

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -1,1 +1,45 @@
-console.log("Hello");
+// -----------------------1------------------------//
+
+// function truncate (str, maxlength) {
+//     if(str.length > maxlength ){
+//         return str.slice(0, maxlength - 3) + "...";
+//     }
+//     return str;
+// }
+
+// console.log(
+//   truncate("Berilgan matn uzunligini tekshiruvchi truncate funksiyasini yozing", 20)
+// );
+
+//-----------------------2---------------------------//
+
+// function randInt(start, end) {
+//   return Math.floor(Math.random() * (end - start + 1)) + start;
+// }
+// console.log(randInt(5, 90));
+
+//-----------------------3--------------------------//
+// function select(s1, s2) {
+//   res = "";
+//   for (const i in s1) {
+//     if (!s1[i].includes()) {
+//         res += s1[i]
+//     }
+//   }
+//   return res
+// }
+// console.log(select("Salom", "Kalom"));
+
+
+//-----------------------4----------------------------//
+// function countChars(str) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         count = Math.count(str[0])
+//     }
+//     return count
+// }   
+
+// console.log(countChars("NAJOT TA'LIM"));
+// console.log(countChars("HELLO WORLD"));
+// console.log(countChars("TESTING"));      
