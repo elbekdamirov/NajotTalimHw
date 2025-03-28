5aaeb15c5b9f83c2fbe377b56b281b0d5ea4eedf
elbekdamirov <elbekmain@gmail.com>
2025-03-28 15:12:09 +0500

    hw9 done

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -21,7 +21,6 @@
 // console.log(calculateWorkingHours("09:00", "17:30"));
 // console.log(calculateWorkingHours("10:00", "09:00"));
 
-
 //-------------------------------2-----------------------------------------------//
 
 // function bankIshKuni(ishKunlari) {
@@ -42,6 +41,53 @@
 // let ishKunlari = 60;
 // console.log(bankIshKuni(ishKunlari));
 
-
 //-------------------------------3-------------------------------------//
-  
+// function findCommonPairs(obj1, obj2) {
+//   if (
+//     typeof obj1 !== "object" ||
+//     typeof obj2 !== "object" ||
+//     obj1 === null ||
+//     obj2 === null
+//   ) {
+//     return obj1 === obj2 ? obj1 : undefined;
+//   }
+
+//   let common = {}
+
+//   for (let key in obj1) {
+//     if (key in obj2) {
+//       let commonValue = findCommonPairs(obj1[key], obj2[key]);
+//       if (commonValue !== undefined) {
+//         common[key] = commonValue;
+//       }
+//     }
+//   }
+
+//   return Object.keys(common).length > 0 ? common : undefined;
+// }
+
+// const jsonData1 = {
+//   user: {
+//     name: "Ali",
+//     age: 25,
+//     address: {
+//       city: "Tashkent",
+//       zip: "100000",
+//     },
+//   },
+//   active: true,
+// };
+
+// const jsonData2 = {
+//   user: {
+//     name: "Ali",
+//     age: 30,
+//     address: {
+//       city: "Tashkent",
+//       zip: "200000",
+//     },
+//   },
+//   active: true,
+// };
+
+// console.log(findCommonPairs(jsonData1, jsonData2));
