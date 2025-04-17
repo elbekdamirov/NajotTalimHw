e70e3c958a928900604d3548d7ef38c7225ed1c8
elbekdamirov <elbekmain@gmail.com>
2025-04-17 12:05:58 +0500

    hw6 almost done

--- NodeJS/lesson6/app.js
+++ NodeJS/lesson6/app.js
@@ -14,3 +14,39 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render(createViewPage("index"), { title: "Main" });
 });
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("telephones"), { title: "Telephones" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("planshets"), { title: "Planshets" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("televisions"), { title: "Televisions" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("laptops"), { title: "Laptops" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("smartwatches"), { title: "Smartwatches" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("ac"), { title: "AC" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("fridges"), { title: "Fridges" });
+});
+
+app.get("/", (req, res) => {
+  res.render(createViewPage("monitors"), { title: "Monitors" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at: http://localhost:${PORT}`);
+});
